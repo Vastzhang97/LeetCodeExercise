3ac508dbf97ffb65c6c244e7a847d421e501f162
WaterZhang97 <31640986+WaterZhang97@users.noreply.github.com>
2019-09-09 01:00:23 +0800

    初始化项目

---
+++ src/main/java/questions/first/hundred/TwoSum.java
@@ -0,0 +1,21 @@
+package questions.first.hundred;
+
+/**
+ * No.1 两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] a = {0, 0};
+        return a;
+    }
+}
