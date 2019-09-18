4234a650ba55aa4569646952ff43f823b8d53ae7
张瀚 <625267847@qq.com>
2019-09-18 08:28:42 +0800

    两数之和

--- src/main/java/questions/first/hundred/TwoSum.java
+++ src/main/java/questions/first/hundred/TwoSum.java
@@ -1,5 +1,9 @@
 package questions.first.hundred;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * No.1 两数之和
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -14,7 +18,8 @@ public class TwoSum {
     private static int[] nums = {3, 2, 4};
 
     public static void main(String[] args) {
-        System.out.println(twoSum(nums, target).toString());
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+        System.out.println(Arrays.toString(twoSum1(nums, target)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -30,4 +35,17 @@ public class TwoSum {
         }
         return null;
     }
+
+    public static int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
 }
